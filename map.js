e0a8e781403204d0c5098050dcc4a81e5faf0643
euniwu <wueunice17@gmail.com>
2018-08-08 09:16:37 -0400

    Adding map and buttons

--- map.js
+++ map.js
@@ -30,3 +30,34 @@ function init() {
 //     init();
 // });
  window.onload=init;
+
+ function panHome(){
+     view.animate({
+        center: ourLoc,
+        duration:2000
+     });
+ }
+
+ function panToLocation(){
+     var countryName= document.getElementById("country-name").value;
+     if (countryName === "") {
+        alert("You didn't enter a country name!");
+        return;
+     }
+     var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+     query = query.replace(/ /g, "%20");
+     alert(query);
+
+     var countryRequest= new XMLHttpRequest();
+     countryRequest.open('GET', query, false);
+     countryRequest.send();
+
+     var lon= 0.0;
+     var lat= 0.0;
+     var location = ol.proj.fromLonLat([lon,lat]);
+     view.animate({
+        center:location,
+        duration: 2000
+     });
+
+ }
